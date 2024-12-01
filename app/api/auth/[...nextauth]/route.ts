a26547883a613573ee230ef85f02a12435164922
ajayi daves <ajayid89@gmail.com>
2024-12-01 11:42:51 +0100

    Update Google provider client secret to use AUTH_SECRET environment variable

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? '',
+      clientSecret: process.env.AUTH_SECRET ?? '',
     }),
   ],
 });
