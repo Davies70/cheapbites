5670c2cd92312d834bfec9cc6ce55f7f36905f8a
ajayi daves <ajayid89@gmail.com>
2024-12-01 11:49:17 +0100

    Update Google provider client secret to use NEXT_PUBLIC_SECRET and add NEXTAUTH_SECRET

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,9 +6,10 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.AUTH_SECRET ?? '',
+      clientSecret: process.env.NEXT_PUBLIC_SECRET ?? '',
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
